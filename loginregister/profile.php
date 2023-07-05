523b9fe10b6dba97430c389a5623b1e73ec4abae
Benjamin Zhang <benjaminzhang1@Benjamin-Linux.localdomain>
2023-07-04 21:20:21 -0400

    Added login and sign up

---
+++ loginregister/profile.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+if (isset($_SESSION["user_id"])) {
+    
+    $mysqli = require __DIR__ . "/database.php";
+    
+    $sql = "SELECT * FROM accounts
+            WHERE id = {$_SESSION["user_id"]}";
+            
+    $result = $mysqli->query($sql);
+    
+    $user = $result->fetch_assoc();
+}
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Homepage</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+<body>
+    <h1>Home</h1>
+    <!--<h3>My orders</h3>
+    <hr>
+    <h3>Favorite orders</h3>-->
+    <?php if (isset($user)): ?>
+        <p>Welcome,  <?= htmlspecialchars($user["username"]) ?></p>
+        <p><a href="logout.php">Log out</a></p>
+        
+    <?php else: ?>
+        <p><a href="login.php">Log in</a> or <a href="signup.html">sign up</a></p>
+    <?php endif; ?>
+    
+</body>
+</html>
+    
---
+++ loginregister/register.php
@@ -0,0 +1,44 @@
+<?php
+if (empty($_POST["username"])) {
+    die("Username is required");
+}
+
+if ($_POST["password"] !== $_POST["con_pass"]) {
+    die("Passwords must match");
+}
+
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+
+$mysqli = require __DIR__ . "/database.php";
+
+$sql = "INSERT INTO accounts (username, password_hash)
+        VALUES (?, ?)";
+        
+$stmt = $mysqli->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("ss",
+                  $_POST["username"],
+                  $password_hash);
+                  
+if ($stmt->execute()) {
+
+    header("Location: login.php");
+    exit;
+    
+} else {
+    
+    if ($mysqli->errno === 1062) {
+        die("username already taken");
+    } else {
+        die($mysqli->error . " " . $mysqli->errno);
+    }
+}
+
+
+
+
+
